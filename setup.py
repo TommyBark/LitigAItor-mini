e2ef569c7da47d521b3ffeb2d6225499d6e7d37f
TommyBark <tomas.turlik@gmail.com>
2024-08-05 13:45:58 +0000

    Update package name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="LitigAItor-mini",
+    name="LitigAItor_mini",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
